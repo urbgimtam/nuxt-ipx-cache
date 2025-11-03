8dd3201cd1c401ac1f8f8fa77dfd6cf75d1d415b
Bruno Mateus <bmateus@vilavitaparc.com>
2025-11-03 17:49:58 +0000

    Proposed fix to [#2](https://github.com/manot40/nuxt-ipx-cache/issues/2), Removed usage of timers and setTimout. This way, the image is deleted if expired, and rebuilt again.

--- src/runtime/server/cache.ts
+++ src/runtime/server/cache.ts
@@ -9,7 +9,6 @@ import fsDriver from 'unstorage/drivers/fs-lite';
  */
 export function createIPXCache(cacheDir: string, defaultTTL = 86400) {
   const store = createStorage<string>({ driver: fsDriver({ base: cacheDir }) });
-  const timers = new Map<string, NodeJS.Timeout>();
   return <CacheStorage>{
     async get(path) {
       const raw = await store.getItemRaw(path);
@@ -19,40 +18,29 @@ export function createIPXCache(cacheDir: string, defaultTTL = 86400) {
       const lastMod = new Date(meta?.expires || 0).getTime();
       const expiresIn = lastMod + defaultTTL * 1000;
       // Check if blob is expired using staled HTTP headers
-      if (Date.now() > expiresIn) return;
-
-      if (!timers.has(path)) {
-        const timeout = setTimeout(() => this.del(path), expiresIn - Date.now());
-        timers.set(path, timeout);
+      if (Date.now() > expiresIn) {
+        // Item is expired, delete it and return nothing.
+        await this.del(path);
+        return;
       }
 
       return { meta, data: new Blob([raw]) };
     },
 
-    async set(path, { data, meta }, ttl = defaultTTL) {
-      if (timers.has(path)) clearTimeout(timers.get(path));
-      const timeout = setTimeout(() => this.del(path), ttl * 1000);
-
+    async set(path, { data, meta }) {
       await Promise.all([
         store.setItemRaw(path, Buffer.isBuffer(data) ? data : await data.arrayBuffer()),
         store.setItem(`${path}.json`, JSON.stringify(meta)),
       ]).catch(console.error);
-
-      timers.set(path, timeout);
     },
 
     async del(path) {
-      if (timers.has(path)) clearTimeout(timers.get(path));
-      timers.delete(path);
-
       const promises = [store.removeItem(path), store.removeItem(`${path}.json`)];
       await Promise.all(promises).catch(() => void 0);
     },
 
     clear() {
       store.clear();
-      for (const v of timers.values()) clearTimeout(v);
-      timers.clear();
     },
   };
 }
@@ -65,7 +53,7 @@ interface CachedData {
 type PayloadData = Omit<CachedData, 'data'> & { data: Blob | Buffer };
 
 interface CacheStorage {
-  set: (path: string, val: PayloadData, ttl?: number) => Promise<void>;
+  set: (path: string, val: PayloadData) => Promise<void>;
   get: (path: string) => Promise<CachedData | undefined>;
   del: (path: string) => Promise<void>;
   clear: () => void;
